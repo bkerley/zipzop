2bdae55d3613647681e7741071404df0f7bd2735
Bryce Kerley <bkerley@brycekerley.net>
2013-05-15 09:01:55 -0400

    initial

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Zipzop::Application.config.secret_key_base = 'adbb4e7f761d1c3b20486c1845fc3c61c2755bc662251a39b6624edce314b162a5c3c81551fe4bc1d7d8a8eaa09f54b7fb36dd81af7356f93f227f5c91243ec5'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Zipzop::Application.config.session_store :cookie_store, key: '_zipzop_session'
